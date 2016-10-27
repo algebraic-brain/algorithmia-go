15698b4c2e0b0d549e8c7c9b667b233f51aebea3
Osman Bineev <bineev@gmail.com>
2016-10-28 00:45:57 +0300

    porting finished (all tests from algorithmia-python)

--- test/datadirectory_test.go
+++ test/datadirectory_test.go
@@ -1,13 +1,49 @@
 package test
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 	"testing"
 
 	algorithmia "github.com/algebraic-brain/algorithmia-go"
 )
 
 var client = algorithmia.NewClient(os.Getenv("ALGORITHMIA_API_KEY"), "")
+
+type some interface{}
+
+func checkDirExists(dd *algorithmia.DataDirectory, onError func(err some)) {
+	if exists, err := dd.Exists(); err != nil {
+		onError(some(err))
+	} else if !exists {
+		onError(some("directory must exist"))
+	}
+}
+
+func checkDirNotExists(dd *algorithmia.DataDirectory, onError func(err some)) {
+	if exists, err := dd.Exists(); err != nil {
+		onError(some(err))
+	} else if exists {
+		onError(some("directory must not exist"))
+	}
+}
+
+func checkFileExists(f *algorithmia.DataFile, onError func(err some)) {
+	if exists, err := f.Exists(); err != nil {
+		onError(some(err))
+	} else if !exists {
+		onError(some("file must exist"))
+	}
+}
+
+func checkFileNotExists(f *algorithmia.DataFile, onError func(err some)) {
+	if exists, err := f.Exists(); err != nil {
+		onError(some(err))
+	} else if exists {
+		onError(some("file must not exist"))
+	}
+}
 
 func TestAcl(t *testing.T) {
 	const myPath = "data://.my/privatePermissions"
@@ -17,7 +53,6 @@ func TestAcl(t *testing.T) {
 
 	if exists, err := dd.Exists(); err != nil {
 		t.Fatal(err)
-
 	} else if exists {
 		if err := dd.ForceDelete(); err != nil {
 			t.Fatal(err)
@@ -60,36 +95,18 @@ func TestDirName(t *testing.T) {
 	}
 	if n != "path" {
 		t.Fatal("'path' expected for directory name, got", n)
-
 	}
 }
 
 func TestDirDoesNotExist(t *testing.T) {
 	dd := client.Dir("data://.my/this_should_never_be_created")
-	if exists, err := dd.Exists(); err != nil {
-	} else if exists {
-		t.Fatal("directory does not exist")
-	}
+
+	checkDirNotExists(dd, func(err some) {
+		t.Fatal(err)
+	})
 }
 
 func TestEmptyDirectoryCreationAndDeletion(t *testing.T) {
-	/*
-	   def test_empty_directory_creation_and_deletion(self):
-	       dd = DataDirectory(self.client, "data://.my/empty_test_directory")
-
-	       if (dd.exists()):
-	           dd.delete(False)
-
-	       self.assertFalse(dd.exists())
-
-	       dd.create()
-	       self.assertTrue(dd.exists())
-
-	       # get rid of it
-	       dd.delete(False)
-	       self.assertFalse(dd.exists())
-	*/
-
 	dd := client.Dir("data://.my/empty_test_directory")
 
 	if exists, err := dd.Exists(); err != nil {
@@ -100,10 +117,96 @@ func TestEmptyDirectoryCreationAndDeletion(t *testing.T) {
 		}
 	}
 
+	checkDirNotExists(dd, func(err some) {
+		t.Fatal(err)
+	})
+
+	err := dd.Create(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checkDirExists(dd, func(err some) {
+		t.Fatal(err)
+	})
+
+	if err := dd.Delete(); err != nil {
+		t.Fatal(err)
+	}
+
+	checkDirNotExists(dd, func(err some) {
+		t.Fatal(err)
+	})
+}
+
+func TestNonemptyDirectoryCreationAndDeletion(t *testing.T) {
+	dd := client.Dir("data://.my/nonempty_test_directory")
+
 	if exists, err := dd.Exists(); err != nil {
 		t.Fatal(err)
 	} else if exists {
-		t.Fatal("directory does not exist")
+		if err := dd.Delete(); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	checkDirNotExists(dd, func(err some) {
+		t.Fatal(err)
+	})
+
+	err := dd.Create(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checkDirExists(dd, func(err some) {
+		t.Fatal(err)
+	})
+
+	f := dd.File("one")
+	checkFileNotExists(f, func(err some) {
+		t.Fatal(err)
+	})
+
+	err = f.Put([]byte("data"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checkFileExists(f, func(err some) {
+		t.Fatal(err)
+	})
+
+	if err := dd.Delete(); err == nil {
+		t.Fatal("removing non-empty directory should fail")
+	}
+
+	checkFileExists(f, func(err some) {
+		t.Fatal(err)
+	})
+
+	checkDirExists(dd, func(err some) {
+		t.Fatal(err)
+	})
+
+	if err := dd.ForceDelete(); err != nil {
+		t.Fatal(err)
+	}
+
+	checkDirNotExists(dd, func(err some) {
+		t.Fatal(err)
+	})
+}
+
+func listFilesSmall(t *testing.T, collectionName string) {
+	dd := client.Dir(collectionName)
+
+	if exists, err := dd.Exists(); err != nil {
+		t.Fatal(err)
+	} else if exists {
+		if err := dd.Delete(); err != nil {
+			t.Fatal(err)
+		}
 	}
 
 	err := dd.Create(nil)
@@ -111,19 +214,191 @@ func TestEmptyDirectoryCreationAndDeletion(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	f1 := dd.File("a")
+	err = f1.Put([]byte("data"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f2 := dd.File("b")
+	err = f2.Put([]byte("data"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	size := 0
+	allFiles := map[string]bool{}
+
+	for f := range dd.Files() {
+		if f.Err != nil {
+			t.Fatal(f.Err)
+		}
+		size += 1
+		allFiles[f.Object.(*algorithmia.DataFile).Path] = true
+	}
+
+	if size != 2 {
+		t.Fatal("number of files listed should be 2")
+	}
+
+	if _, ok := allFiles[f1.Path]; !ok {
+		t.Fatal("file 'a' not found in collection")
+	}
+
+	if _, ok := allFiles[f2.Path]; !ok {
+		t.Fatal("file 'b' not found in collection")
+	}
+
+	if err := dd.ForceDelete(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestListFilesSmallWithoutTrailingSlash(t *testing.T) {
+	listFilesSmall(t, "data://.my/test_list_files_small")
+}
+
+func TestListFilesSmallWithTrailingSlash(t *testing.T) {
+	listFilesSmall(t, "data://.my/test_list_files_small/")
+}
+
+func TestListFolders(t *testing.T) {
+	dd := client.Dir("data://.my/")
+	dirName := ".my/test_list_directory"
+	testDir := client.Dir("data://" + dirName)
+
+	if exists, err := testDir.Exists(); err != nil {
+		t.Fatal(err)
+	} else if exists {
+		if err := testDir.Delete(); err != nil {
+			t.Fatal(err)
+		}
+	}
+	allFolders := map[string]bool{}
+
+	for f := range dd.Dirs() {
+		if f.Err != nil {
+			t.Fatal(f.Err)
+		}
+		allFolders[f.Object.(*algorithmia.DataDirectory).Path] = true
+	}
+
+	if _, ok := allFolders[dirName]; ok {
+		t.Fatal("directory '" + dirName + "' should not be found in collection")
+	}
+
+	err := testDir.Create(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for f := range dd.Dirs() {
+		if f.Err != nil {
+			t.Fatal(f.Err)
+		}
+		allFolders[f.Object.(*algorithmia.DataDirectory).Path] = true
+	}
+
+	if _, ok := allFolders[dirName]; !ok {
+		t.Fatal("directory '" + dirName + "' not found in collection")
+	}
+
+	err = testDir.ForceDelete()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDataObject(t *testing.T) {
+	d := client.Dir("data://foo")
+	if !d.IsDir() {
+		t.Fatal("object expected to be a directory")
+	}
+	if d.IsFile() {
+		t.Fatal("object is not expected to be a file")
+	}
+	if d.Type() != algorithmia.Directory {
+		t.Fatal("object expected to be a directory")
+	}
+}
+
+func TestListFilesWithPaging(t *testing.T) {
+	/*
+	   def test_list_files_with_paging(self):
+	       NUM_FILES = 1100
+	       EXTENSION = '.txt'
+
+	       dd = DataDirectory(self.client, 'data://.my/pythonLargeDataDirList')
+	       if not dd.exists():
+	           dd.create()
+
+	           for i in range(NUM_FILES):
+	               dd.file(str(i) + EXTENSION).put(str(i))
+
+	       seenFiles = [False] * NUM_FILES
+	       numFiles = 0
+
+	       for f in dd.files():
+	           numFiles += 1
+	           name = f.getName()
+	           index = int(name[:-1 * len(EXTENSION)])
+	           seenFiles[index] = True
+
+	       allSeen = True
+	       for cur in seenFiles:
+	           allSeen = (allSeen and cur)
+
+	       self.assertEqual(NUM_FILES, numFiles)
+	       self.assertTrue(allSeen)
+	*/
+	const (
+		NUM_FILES = 1100
+		EXTENSION = ".txt"
+	)
+	dd := client.Dir("data://.my/golangLargeDataDirList")
 	if exists, err := dd.Exists(); err != nil {
 		t.Fatal(err)
 	} else if !exists {
-		t.Fatal("directory must exist")
+		if err := dd.Create(nil); err != nil {
+			t.Fatal(err)
+		}
+		for i := 0; i < NUM_FILES; i++ {
+			fname := fmt.Sprint(i)
+			err := dd.File(fname + EXTENSION).Put([]byte(fname))
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
 	}
 
-	if err := dd.Delete(); err != nil {
-		t.Fatal(err)
+	seenFiles := make([]bool, NUM_FILES)
+	numFiles := 0
+
+	for f := range dd.Files() {
+		if f.Err != nil {
+			t.Fatal(f.Err)
+		}
+		numFiles++
+		name, err := f.Object.(*algorithmia.DataFile).Name()
+		if err != nil {
+			t.Fatal(err)
+		}
+		index, err := strconv.Atoi(name[:len(name)-len(EXTENSION)])
+		if err != nil {
+			t.Fatal(err)
+		}
+		seenFiles[index] = true
+	}
+	allSeen := true
+	for _, cur := range seenFiles {
+		allSeen = (allSeen && cur)
 	}
 
-	if exists, err := dd.Exists(); err != nil {
-		t.Fatal(err)
-	} else if exists {
-		t.Fatal("directory does not exist")
+	if numFiles != NUM_FILES {
+		t.Fatal("numFiles != NUM_FILES")
+	}
+
+	if !allSeen {
+		t.Fatal("!allSeen")
 	}
 }
